da3436b9bdccd1c213b08543085dc308afded325
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-10-18 22:24:31 +0200

    adding graphql generator to generate hooks

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,43 +1,20 @@
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button
-} from "@chakra-ui/core";
+import { Box, Button } from "@chakra-ui/core";
 import { Formik, Form } from "formik";
 import React from "react";
-import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-mutation Register ($username: String!, $password: String!) {
-    register(options: { username: $username, password: $password }) {
-          errors{
-        field
-        message
-      }
-      user {
-        id
-        username
-      }
-    }
-  }
-  `;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUTATION);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={values => {
-          values => console.log(values);
-          return register(values);
+        onSubmit={async values => {
+          const response = await register(values);
         }}
       >
         {({ values, handleChange, isSubmitting }) =>
